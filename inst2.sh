241db3156edea895cee76bb840624583d8de9258
GitWloda <66310192+GitWloda@users.noreply.github.com>
2021-12-17 12:32:37 +0100

    Update inst2.sh

--- inst2.sh
+++ inst2.sh
@@ -7,9 +7,9 @@ echo LANG=it_IT.UTF-8 > etc/locale.conf
 echo KEYMAP=it > /etc/vsconsole.conf
 echo virt > /etc/hostname
 echo "127.0.0.1 localhost.localdomain virt" > /etc/hosts
-mkdir /mnt/boot/efi
-mount /dev/sda1 /mnt/boot/efi
-grub-install --target=x86_64-efi --efi-directory=/mnt/boot/efi --bootloader-id=GRUB --removable
+mkdir /boot/efi
+mount /dev/sda1 /boot/efi
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --removable
 grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable NetworkManager
 useradd -m -g users -G wheel -s /bin/bash virt
