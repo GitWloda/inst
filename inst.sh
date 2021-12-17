14491b63a5d4476a73fea80a711a74d63cb61ce2
GitWloda <66310192+GitWloda@users.noreply.github.com>
2021-12-17 11:34:10 +0100

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -4,7 +4,7 @@ cfdisk
 mkfs.fat -F 32 /dev/sda1
 mkfs.ext4 /dev/sda2
 mount /dev/sda2 /mnt
-pacstrap /mnt base linux linux-firmware
+pacstrap /mnt base linux linux-firmware grub efibootmgr amd-ucode networkmanager vim sudo 
 genfstab -U /mnt >> /mnt/etc/fstab
 chmod 777 ./inst2.sh
 cp ./inst2.sh /mnt
