28d502507341f3f0d54eae4f4f746b2b5d3558dc
GitWloda <66310192+GitWloda@users.noreply.github.com>
2021-12-17 11:27:54 +0100

    Update inst2.sh

--- inst2.sh
+++ inst2.sh
@@ -8,17 +8,17 @@ echo KEYMAP=it > /etc/vsconsole.conf
 echo virt > /etc/hostname
 echo "127.0.0.1 localhost.localdomain virt" > /etc/hosts
 passwd --stdin
-pacman -S grub --stdin
-pacman -S efibootmgr --stdin
+pacman -S grub 
+pacman -S efibootmgr 
 mkdir /mnt/boot/efi
 mount /dev/sda1 /mnt/boot/efi
 grub-install --target=x86_64-efi --efi-directory=/mnt/boot/efi --bootloader-id=GRUB --removable
 grub-mkconfig -o /boot/grub/grub.cfg
-pacman -S amd-ucode --stdin
-pacman -S networkmanager --stdin
+pacman -S amd-ucode 
+pacman -S networkmanager 
 systemctl enable NetworkManager
 useradd -m -g users -G wheel -s /bin/bash virt
 passwd virt --stdin
-sudo pacman -S vim sudo --stdin
+sudo pacman -S vim sudo 
 EDITOR=vim visudo
 exit
