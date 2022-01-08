ba0c5d5c4606fb42f97ec1eeea6e69028edb9b1f
GitWloda <66310192+GitWloda@users.noreply.github.com>
2022-01-08 20:28:52 +0100

    Update inst2.sh

--- inst2.sh
+++ inst2.sh
@@ -8,7 +8,7 @@ echo KEYMAP=it > /etc/vsconsole.conf
 echo virt > /etc/hostname
 echo "127.0.0.1 localhost.localdomain virt" > /etc/hosts
 mkdir /boot/efi
-mount /dev/sda1 /boot/efi
+mount /dev/vda1 /boot/efi
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --removable
 grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable NetworkManager
