b2dafe6ca77e1a2cdff36194142556aafb02f6bf
GitWloda <66310192+GitWloda@users.noreply.github.com>
2022-01-08 20:28:36 +0100

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -1,9 +1,9 @@
 loadkeys it
 timedatactl set-ntp true
 cfdisk
-mkfs.fat -F 32 /dev/sda1
-mkfs.ext4 /dev/sda2
-mount /dev/sda2 /mnt
+mkfs.fat -F 32 /dev/vda1
+mkfs.ext4 /dev/vda2
+mount /dev/vda2 /mnt
 pacstrap /mnt base linux linux-firmware grub efibootmgr amd-ucode networkmanager vim sudo virtualbox-guest-utils
 genfstab -U /mnt >> /mnt/etc/fstab
 chmod 777 ./inst2.sh
